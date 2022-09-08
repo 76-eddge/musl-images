bd5e94bafd2336e77af709b4bb781b8e641a556e
matthew <nny01101101@yahoo.com>
2022-09-08 15:30:38 -0500

    Attempting to build Ubuntu test image

--- muslCross/setMuslLinux.ps1
+++ muslCross/setMuslLinux.ps1
@@ -1,6 +1,7 @@
 param($Arch)
 
 $Environment
+$Version = "11.3.0"
 
 Switch ($Arch) {
 	"x86_64" {$Environment = "x86_64-linux-musl"}
@@ -10,9 +11,10 @@ Switch ($Arch) {
 	"powerpc64le" {$Environment = "powerpc64le-linux-musl"}
 	default {throw "Invalid architecture selection. Valid selections : x86_64, aarch, arm, i686, powerpc64le"}
 } 
-
-$env:PATH = "/opt/$Environment/bin:" + $env:PATH
+$env:LIBRARY_PATH= "/opt/${Environment}/lib/${Environment}/${Version}:/opt/${Environment}/${Environment}/lib:" + $env:LIBRARY_PATH
+$env:LD_LIBRARY_PATH= "/opt/${Environment}/lib/${Environment}/${Version}:/opt/${Environment}/${Environment}/lib:" + $env:LD_LIBRARY_PATH
+$env:PATH = "/opt/${Environment}/bin:" + $env:PATH
 # TODO: Do we need to add any includes here for the default search path?
-$env:CPATH = "/opt/$Environment/$Environment/include"
-$env:CXX = "$Environment-g++"
-$env:CC = "$Environment-gcc"
+$env:CPATH = "/opt/${Environment}/${Environment}/include"
+$env:CXX = "${Environment}-g++"
+$env:CC = "${Environment}-gcc"
